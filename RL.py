b0722ca379f46c84edfa938af7b7b07b977715fa
Tae Soo Kim <tkim60@jhu.edu>
2016-11-14 18:38:51 -0500

    updates to World.py

--- RL.py
+++ RL.py
@@ -1,13 +1,13 @@
 import pdb
 from World import *
-from Models import *
+#from Models import *
 import numpy as np 
 
 def train(model,env,exp_replay,epoch,action_space,epsilon,batch_size):
   model_path_out_path = '/home/tk/dev/unrealcv/src/model_weights/'
-  for ep in epoch:
+  for ep in range(0,epoch):
     loss = 0.0
-    evn.reset()
+    env.reset()
     objective_complete = False
     win_count = 0
     
@@ -32,11 +32,11 @@ def train(model,env,exp_replay,epoch,action_space,epsilon,batch_size):
         win_count += 1
 
       # store experience
-      exp_replay.remember(frame_prev,action,reward,frame,objective_complete)
+      #exp_replay.remember(frame_prev,action,reward,frame,objective_complete)
 
       # learn
-      inputs, targets = exp_replay.get_batch(model, batch_size=batch_size)
-      loss += model.train_on_batch(inputs,targets)[0]
+      #inputs, targets = exp_replay.get_batch(model, batch_size=batch_size)
+      #loss += model.train_on_batch(inputs,targets)[0]
 
     print "Epoch {%03d}/{%d} | Loss {%.6f} | Win count {}".format(ep, loss, win_count)
 
@@ -44,15 +44,18 @@ def train(model,env,exp_replay,epoch,action_space,epsilon,batch_size):
 if __name__ == "__main__":
   ## TRAINING PARAMETERS
   EPOCH = 150
-  ACTION_SPACE = 4 # go_front, go_back, turn_left, turn_right
-  EPSILON = 0.1 # Exploration
+  ACTION_SPACE = 4 # 0:go_front, 1:go_back, 2:turn_left, 3:turn_right
+  EPSILON = 1.1 # Exploration
   BATCH_SIZE = 128
 
   # get model
   env = World()
-  exp_replay = ExperienceReplay(max_memory=max_memory)
+  #pdb.set_trace()
+  #exp_replay = ExperienceReplay(max_memory=max_memory)
+  train(None,env,None,EPOCH,ACTION_SPACE,EPSILON,BATCH_SIZE)
   #model = UENet()
 
+  """
   train(model=model,
         env=env,
         exp_replay=exp_replay,
@@ -61,5 +64,5 @@ if __name__ == "__main__":
         epsilon=EPSILON,
         batch_size=BATCH_SIZE)
 
-
+  """
   
--- World.py
+++ World.py
@@ -1,5 +1,10 @@
 import sys
 from unrealcv import client
+import matplotlib.pyplot as plt
+import pdb
+import cv2
+from math import cos,sin,radians
+import numpy as np
 
 ## World object. This represents the state of the UnrealEngine game environment.
 class World(object):
@@ -12,14 +17,113 @@ class World(object):
     res = client.request('vget /unrealcv/status')
     print res
   
-    pass
 
   def observe(self):
     ## get current frame
-    pass
+    img_out_path = '/home/tk/dev/unrealcv/src/temp_imgs/img.png'
+    res = client.request('vget /camera/0/lit ' + img_out_path )
+    img = cv2.imread(img_out_path)
+    return img
 
   def act(self,action):
-    pass
+    ## TODO: Write routine to move the actor
+    # 0:go_front, 1:go_back, 2:turn_left, 3:turn_right
+
+    ## Try to move -> collision check -> undo move
+    if action == 0:
+      self._move_forward()
+    elif action == 1:
+      self._move_backward()
+    elif action == 2:
+      self._rotate_left()
+    elif action == 3:
+      self._rotate_right()
+    
+    return self.observe(), self._get_reward(), self._is_over()
 
   def reset(self):
+    ## TODO: Write routine to restart the game
+
+    ## The game can reset itself once the trigger is set off.
     pass
+
+  def _get_reward(self):
+    ## TODO: design reward
+    # +10: task_complete, -0.1: every step, -1: collision
+    if self._is_over():
+      return 10
+    elif self._in_collision():
+      return -1
+    else:
+      return -0.1
+
+    
+    return 0
+
+  def _is_over(self):
+    ## TODO: implement game over logic
+    ## If in view and close enough
+
+    ## Maybe listen to an event that is triggered by the trigger box in game.
+    return False
+
+  def _in_collision(self):
+    ## TODO: Listen to collision nevents
+    pass
+
+  ## Actor Manipulation Methods
+  def _move_forward(self,id=0):
+    ## This moves the camera forward by a fixed step size
+    step_size = 50
+
+    cur_pos = self._get_camera_pos(id)
+    cur_rot = self._get_camera_rotation(id)
+    R = self._toRotationMatrix(cur_rot[0],cur_rot[1],cur_rot[2])
+    new_pos = np.dot(R,np.transpose(np.array((step_size,0,0)))) + np.array(cur_pos)
+    res = client.request('vset /camera/%d/location %f %f %f'%(id,new_pos[0],new_pos[1],new_pos[2]))
+    return True
+
+  def _move_backward(self,id=0):
+    ## This moves the camera forward by a fixed step size
+    step_size = -50
+
+    cur_pos = self._get_camera_pos(id)
+    cur_rot = self._get_camera_rotation(id)
+    R = self._toRotationMatrix(cur_rot[0],cur_rot[1],cur_rot[2])
+    new_pos = np.dot(R,np.transpose(np.array((step_size,0,0)))) + np.array(cur_pos)
+    res = client.request('vset /camera/%d/location %f %f %f'%(id,new_pos[0],new_pos[1],new_pos[2]))
+    return True
+
+  ## Camera Accessor Methods
+  def _get_camera_pos(self,id=0):
+    res = client.request('vget /camera/%d/location'%id)
+    x,y,z = res.split(' ')
+    return float(x), float(y), float(z)
+
+  def _get_camera_rotation(self,id=0):
+    res = client.request('vget /camera/%d/rotation'%id)
+    pitch,yaw,roll = res.split(' ')
+    return float(pitch), float(yaw), float(roll)
+
+  def _rotate_right(self,id=0):
+    step_size = 10
+    cur_rot = self._get_camera_rotation(id)
+    new_rot = (cur_rot[0],cur_rot[1]+step_size,cur_rot[2])
+    res = client.request('vset /camera/%d/rotation %f %f %f'%(id,new_rot[0],new_rot[1],new_rot[2]))
+
+  def _rotate_left(self,id=0):
+    step_size = -10
+    cur_rot = self._get_camera_rotation(id)
+    new_rot = (cur_rot[0],cur_rot[1]+step_size,cur_rot[2])
+    res = client.request('vset /camera/%d/rotation %f %f %f'%(id,new_rot[0],new_rot[1],new_rot[2]))
+
+  def _toRotationMatrix(self,pitch,yaw,roll):
+    p = radians(pitch)
+    y = radians(yaw)
+    r = radians(roll)
+    Rz = np.array([[cos(y),-sin(y),0],[sin(y),cos(y),0],[0,0,1]])
+    Ry = np.array([[cos(p),0,sin(p)],[0,1,0],[-sin(p),0,cos(p)]])
+    Rx = np.array([[1,0,0],[0,cos(r),-sin(r)],[0,sin(r),cos(r)]])
+    RzRy = np.dot(Rz,Ry)
+    RzRyRx = np.dot(RzRy,Rx)
+    return RzRyRx
